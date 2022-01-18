b932e69d98fa8d5bd16cda9092be5c448945332b
Dubjunk <Bam2601@gmail.com>
2022-01-18 14:43:56 +0100

    added role management

--- entry.py
+++ entry.py
@@ -8,11 +8,21 @@ class entry(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    # Message on startup
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('entry running...')
+        print('--------------------')
+
     # Events
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        print(member)
-        await member.send('Hallo!')
+        print('Member joined: {0}'.format(member))
+        line1 = '**Herzlichen Willkommen auf dem Discord Server von LiftOff Industries {0.name}.**'.format(member)
+        line2 = 'Um vollen Zugang zu unserem öffentlichen Bereich zu erhalten, musst du zuerst unsere Server Regeln akzeptieren.'
+        line3 = 'Besuche dazu folgenden Channel <#841755767988289615>, lies dir die Regeln gemütlich durch und bestätige sie.'
+        line4 = 'Lifty'
+        await member.send(line1 + '\n\n' + line2 + '\n' + line3 + '\n\n' + line4)
 
 # define extension
 def setup(bot):
--- main.py
+++ main.py
@@ -3,19 +3,22 @@ import os
 from discord.ext import commands
 
 # Defines
-intents = discord.Intents(members=True)
+intents = discord.Intents.default()
+intents.members = True
+intents.reactions = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 # Message on startup
 @bot.event
 async def on_ready():
     print('Bot running...')
-    print('Name: {0.user.name}'.format(bot))
+    print('Name: {0.user}'.format(bot))
     print('ID: {0.user.id}'.format(bot))
     print('--------------------')
-    await bot.change_presence(activity=discord.Game("Making Money"))
+    await bot.change_presence(activity=discord.Game("LiftOff Industries"))
 
 # Load Extensions
 bot.load_extension('entry')
+bot.load_extension('roles')
 
-bot.run('Token')
+bot.run('ODMzNDI5OTA0NzYyMDc3MjA2.YHyOFQ.7gJHZ_fJUHIt4rVaLnqypwsGv_g')
